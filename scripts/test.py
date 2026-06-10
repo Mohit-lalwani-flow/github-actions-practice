dc31d8bf0590ac50a40f50e7b51662a970be394f
Mohit Lalwani <mohitlalwaniwork@gmail.com>
2026-06-10 07:32:55 +0000

    Added intentional error to scripts/test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,8 @@
-print("This is python test file")
+# print("This is python test file")
+
+import sys
+
+print("Testing pipeline failure...")
+# This forces the script to exit with code 1, causing the pipeline to fail
+# Intentionally break the script (Go Red 🔴)
+sys.exit(1) 
